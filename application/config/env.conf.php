d36786d4ee1ba361c58cfd25d084a0df096bc8a3
humingbird <H.Osada04@gmail.com>
2012-03-25 18:08:00 +0900

    configファイル追加

---
+++ application/config/env.conf.php
@@ -0,0 +1,5 @@
+<?php
+
+class EnvConf{
+	public static $env = 'default';
+}
---
+++ application/config/web.conf.template.php
@@ -0,0 +1,8 @@
+<?php
+class WebConf{
+	public static $home_dir = '';
+	
+	//smarty
+	public static $templates_dir = '';
+	public static $templates_c_dir = '';
+}
